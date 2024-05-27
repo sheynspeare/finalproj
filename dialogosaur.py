f3c5b81b3564a26e0d1b0a6920ff907bf076fdc3
Shayne Yanson <99411580+sheynspeare@users.noreply.github.com>
2024-05-27 20:00:25 +0800

    Update dialogosaur.py

--- dialogosaur.py
+++ dialogosaur.py
@@ -1,22 +1,40 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
 import google.generativeai as genai
 
-API_KEY = 'AIzaSyDjJ0JSkAmSzN6kYRdVgdjqkniNa8Fsy9k'
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-genai.configure(
-    api_key = API_KEY
-)
+## loads Gemini and get response
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(history=[])
 
-model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat(history=[])
-instruction = "Respond as if you're explaining things to a ten-year-old kid"
+def get_gemini_response(question):
+    response=chat.send_message(question, stream=True)
 
-while (True):
-  question = input("You: ")
+##initialize streamlit
+st.set_page_config(page_title="Q&A Demo")
+st.header("Ask Dialogosaur!")
 
-  if(question.strip() == ''):
-    break
+##initialize sesh state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history']=[]
 
-  response = chat.send_message(instruction + question)
-  print(f"Dialogosaur: {response.text}")
-  print('\n')
-  instruction = ''
+input = st.text_input("Input: ", key="input")
+submit=st.button("Send")
+
+if submit and input:
+    response=get_gemini_response(input)
+
+    ## add user query and response to sesh chat history
+    st.session_state['chat_history'].append(("You: ", input))
+    st.subheader("The response: ")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Dialogosaur: ", chunk.text))
+st.subheader("/n-------------------/n  The Chat history is")
+
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
